7327fed923eaaaa9ecbd6d6c559b26380edbfdc0
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-08-18 12:45:14 +0700

    fix: remove unnecessary import package

--- lib/presentation/routes/route.dart
+++ lib/presentation/routes/route.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 import 'package:synapsis_intern/presentation/routes/route_name.dart';
-import 'package:synapsis_intern/presentation/screens/home/home_page.dart';
+import 'package:synapsis_intern/presentation/screens/home/first_page.dart';
 
 class Routes {
   const Routes._();
